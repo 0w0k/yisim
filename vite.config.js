08e3d4b3f206644251173266a9661be55882ba3b
Kiven <412386861@qq.com>
2025-05-09 12:28:57 +0800

    build cloudflare

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import pkg from './package.json';
 
+const DEPLOY_ENV = process.env.DEPLOY;
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/yisim',
+  base: DEPLOY_ENV === 'githubpages' ? 'yisim' : '/',
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version),
   },
